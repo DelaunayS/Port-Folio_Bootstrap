bf525645cd1c94e07aacc196b0ba6ecfbd77ef86
DelaunayS <sebolix@hotmail.fr>
2021-02-01 20:42:45 +0100

    ajout de l'accordeon de TP

---
+++ js/app_index.js
@@ -0,0 +1,23 @@
+
+// ouvre le mail dans une fenêtre alert
+let mail= document.getElementById("contact_mail")
+
+mail.onclick =function () {alert ("sebastien.delaunay44@gmail.com")};
+
+// ouvre la page WebMaternelle
+let page1= document.getElementById("page1")
+page1.onclick = function (){
+    window.open("https://delaunays.github.io/WebMaternelle/", "_blank");
+};
+
+// ouvre la page Visualiser partie echecs
+let page2= document.getElementById("page2")
+page2.onclick = function (){
+    window.open("https://delaunays.github.io/WebVisualiserPartie/", "_blank");
+};
+
+// ouvre la page de l'accordeon de TP
+let page3= document.getElementById("page3")
+page3.onclick = function (){
+    window.open("https://delaunays.github.io/TPReact//", "_blank");
+};
