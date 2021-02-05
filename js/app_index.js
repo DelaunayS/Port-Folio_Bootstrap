6019ca205076d47b6186e87fee905a5e9510e7ef
DelaunayS <sebolix@hotmail.fr>
2021-02-05 17:32:06 +0100

    Ajout de la partie Mes parties d'Echecs et réorganisation de l'ordre des projets

--- js/app_index.js
+++ js/app_index.js
@@ -4,20 +4,20 @@ let mail= document.getElementById("contact_mail")
 
 mail.onclick =function () {alert ("sebastien.delaunay44@gmail.com")};
 
-// ouvre la page WebMaternelle
+// ouvre la page Accordéon de TP
 let page1= document.getElementById("page1")
 page1.onclick = function (){
+    window.open("https://delaunays.github.io/TPReact/", "_blank");
+};
+
+// ouvre la page WebMaternelle
+let page2= document.getElementById("page2")
+page2.onclick = function (){
     window.open("https://delaunays.github.io/WebMaternelle/", "_blank");
 };
 
-// ouvre la page Visualiser partie echecs
-let page2= document.getElementById("page2")
-page2.onclick = function (){
-    window.open("https://delaunays.github.io/WebVisualiserPartie/", "_blank");
-};
-
-// ouvre la page Calculatrice
+// ouvre la page Mes parties d'échecs
 let page3= document.getElementById("page3")
 page3.onclick = function (){
-    window.open("https://delaunays.github.io/WebCalculator/", "_blank");
+    window.open("https://delaunays.github.io/SelectionParties/", "_blank");
 };
